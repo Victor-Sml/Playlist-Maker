d910987caf67ae4c67bf67d49d7ea823fc292749
Viktor <vcdyj4hu8@yandex.ru>
2022-12-02 13:53:29 +0300

    Спринт 9

--- app/src/main/java/com/victor_sml/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/victor_sml/playlistmaker/SearchActivity.kt
@@ -9,20 +9,20 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     //Переменя для хранения поискового запроса
     private var searchQuery: String? = null
+    private lateinit var inputEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
-        val inputEditText = findViewById<EditText>(R.id.inputEditText)
-
-        inputEditText.setText(searchQuery)
+        inputEditText = findViewById<EditText>(R.id.inputEditText)
 
         val searchTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -50,13 +50,14 @@ class SearchActivity : AppCompatActivity() {
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putString(SEARCH_INPUT, searchQuery)
         super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_INPUT, searchQuery)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         searchQuery = savedInstanceState.getString(SEARCH_INPUT, "")
+        inputEditText.setText(searchQuery)
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
