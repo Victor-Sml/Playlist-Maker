f4937abc2a90fa496ad1695cac81f478aefd2d4a
Viktor <vcdyj4hu8@yandex.ru>
2023-01-29 22:38:07 +0300

    Спринт 9

--- app/src/main/java/com/victor_sml/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/victor_sml/playlistmaker/SearchActivity.kt
@@ -9,6 +9,8 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SearchActivity : AppCompatActivity() {
     //Переменя для хранения поискового запроса
@@ -21,6 +23,7 @@ class SearchActivity : AppCompatActivity() {
 
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
         val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.searchToolbar)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview_tracklist)
         inputEditText = findViewById(R.id.inputEditText)
 
         val searchTextWatcher = object : TextWatcher {
@@ -40,6 +43,9 @@ class SearchActivity : AppCompatActivity() {
             hideSoftInput()
             currentFocus?.clearFocus()
         }
+
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = TrackAdapter(getTrackList())
 
         toolbar.setNavigationOnClickListener {
             this.finish()
@@ -77,6 +83,45 @@ class SearchActivity : AppCompatActivity() {
             getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
         inputMethodManager?.hideSoftInputFromWindow(view?.windowToken, 0)
     }
+
+    private fun getTrackList() =
+        arrayListOf(
+            Track(
+                "Smells Like Teen Spirit",
+                "Nirvana",
+                "5:01",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/" +
+                        "7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Billie Jean",
+                "Michael Jackson",
+                "4:35",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/" +
+                        "3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Stayin' Alive",
+                "Bee Gees",
+                "4:10",
+                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/" +
+                        "1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Whole Lotta Love",
+                "Led Zeppelin",
+                "5:33",
+                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/" +
+                        "7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Sweet Child O'Mine",
+                "Guns N' Roses",
+                "5:03",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/" +
+                        "a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+            )
+        )
 
     companion object {
         const val SEARCH_INPUT = "SEARCH_INPUT"
---
+++ app/src/main/java/com/victor_sml/playlistmaker/TrackAdapter.kt
@@ -0,0 +1,44 @@
+package com.victor_sml.playlistmaker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TrackAdapter(private val tracklist: ArrayList<Track>) :
+    RecyclerView.Adapter<TrackAdapter.TrackViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder =
+        TrackViewHolder(parent)
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(tracklist[position])
+    }
+
+    override fun getItemCount(): Int = tracklist.size
+
+    class TrackViewHolder(parentView: ViewGroup) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parentView.context)
+            .inflate(R.layout.tracklist_item_view, parentView, false)
+    ) {
+        private val artwork: ImageView = itemView.findViewById(R.id.iv_artwork)
+        private val trackName: TextView = itemView.findViewById(R.id.tv_track_name)
+        private val artistName: TextView = itemView.findViewById(R.id.tv_artistName)
+        private val trackTime: TextView = itemView.findViewById(R.id.tv_track_time)
+
+        fun bind(track: Track) {
+            trackName.text = track.trackName
+            artistName.text = track.artistName
+            trackTime.text = track.trackTime
+
+            Glide.with(itemView.context).load(track.artworkUrl100)
+                .placeholder(R.drawable.default_artwork)
+                .centerCrop()
+                .transform(RoundedCorners(2))
+                .into(artwork)
+        }
+    }
+}
