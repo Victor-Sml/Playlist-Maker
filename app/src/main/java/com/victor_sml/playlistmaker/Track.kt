5836ba339b2de3a0a7e9a75ebea37696480790f7
Viktor <vcdyj4hu8@yandex.ru>
2023-03-30 21:46:12 +0300

    Спринт 13.

--- app/src/main/java/com/victor_sml/playlistmaker/Track.kt
+++ app/src/main/java/com/victor_sml/playlistmaker/Track.kt
@@ -1,6 +1,5 @@
 package com.victor_sml.playlistmaker
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.TypeAdapter
 import com.google.gson.annotations.JsonAdapter
@@ -8,9 +7,11 @@ import com.google.gson.annotations.SerializedName
 import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonToken
 import com.google.gson.stream.JsonWriter
+import kotlinx.parcelize.Parcelize
 import java.text.SimpleDateFormat
 import java.util.Locale
 
+@Parcelize
 data class Track(
     val artistName: String?,
     val trackName: String?,
@@ -24,41 +25,6 @@ data class Track(
     val primaryGenreName: String?,
     val country: String?
 ) : RecyclerItemType, Parcelable {
-    private constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(artistName)
-        parcel.writeString(trackName)
-        parcel.writeString(artworkUrl100)
-        parcel.writeString(trackTime)
-        parcel.writeString(collectionName)
-        parcel.writeString(releaseDate)
-        parcel.writeString(primaryGenreName)
-        parcel.writeString(country)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Track> {
-        override fun createFromParcel(parcel: Parcel): Track {
-            return Track(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Track?> {
-            return arrayOfNulls(size)
-        }
-    }
 
     fun getCoverArtwork() = artworkUrl100?.replaceAfterLast('/', "512x512bb.jpg")
 
