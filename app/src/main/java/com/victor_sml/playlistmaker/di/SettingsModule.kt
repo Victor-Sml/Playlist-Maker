f88a79cc2464d477667b12ba60215399545e890c
Viktor <vcdyj4hu8@yandex.ru>
2023-05-09 14:30:27 +0300

    Спринт 17.

--- app/src/main/java/com/victor_sml/playlistmaker/di/SettingsModule.kt
+++ app/src/main/java/com/victor_sml/playlistmaker/di/SettingsModule.kt
@@ -1,6 +1,5 @@
 package com.victor_sml.playlistmaker.di
 
-import android.content.Context
 import com.victor_sml.playlistmaker.settings.data.SettingsRepositoryImpl
 import com.victor_sml.playlistmaker.settings.data.api.SettingsStorage
 import com.victor_sml.playlistmaker.settings.data.storage.SharedPrefSettingStorage
@@ -9,12 +8,11 @@ import com.victor_sml.playlistmaker.settings.domain.api.SettingsInteractor
 import com.victor_sml.playlistmaker.settings.domain.api.SettingsRepository
 import com.victor_sml.playlistmaker.settings.ui.stateholders.SettingsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
 
 val settingsModule = module {
-    viewModel { (context: Context) ->
-        SettingsViewModel(get(), get(){ parametersOf(context)})
+    viewModel {
+        SettingsViewModel(get(), get())
     }
 
     single<SettingsInteractor> {
--- app/src/main/java/com/victor_sml/playlistmaker/di/SharingModule.kt
+++ app/src/main/java/com/victor_sml/playlistmaker/di/SharingModule.kt
@@ -1,6 +1,5 @@
 package com.victor_sml.playlistmaker.di
 
-import android.content.Context
 import com.victor_sml.playlistmaker.sharing.data.ExternalNavigatorImpl
 import com.victor_sml.playlistmaker.sharing.data.SharingRepositoryImpl
 import com.victor_sml.playlistmaker.sharing.data.api.StringSource
@@ -9,16 +8,15 @@ import com.victor_sml.playlistmaker.sharing.domain.SharingInteractorImpl
 import com.victor_sml.playlistmaker.sharing.domain.api.ExternalNavigator
 import com.victor_sml.playlistmaker.sharing.domain.api.SharingInteractor
 import com.victor_sml.playlistmaker.sharing.domain.api.SharingRepository
-import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
 
 val sharingModule = module {
-    single<SharingInteractor> { (context: Context) ->
-        SharingInteractorImpl(get() { parametersOf(context) }, get())
+    single<SharingInteractor> {
+        SharingInteractorImpl(get(), get())
     }
 
-    single<ExternalNavigator> { (context: Context) ->
-        ExternalNavigatorImpl(context)
+    single<ExternalNavigator> {
+        ExternalNavigatorImpl(get())
     }
 
     single<SharingRepository> {
