f459cbe613c783ef4fb7118e82e97beee695c4eb
Viktor <vcdyj4hu8@yandex.ru>
2023-06-24 18:04:56 +0300

    Спринт 20.

--- app/src/main/java/com/victor_sml/playlistmaker/search/ui/view/SearchFragment.kt
+++ app/src/main/java/com/victor_sml/playlistmaker/search/ui/view/SearchFragment.kt
@@ -3,7 +3,6 @@ package com.victor_sml.playlistmaker.search.ui.view
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
